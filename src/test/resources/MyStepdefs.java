fe838c6fb4174b6e7bebe182c4f92179cf7c4e9e
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-12 22:47:20 +0700

    Сгенерировали код по спецификации (comparematrix)

---
+++ src/test/resources/MyStepdefs.java
@@ -0,0 +1,31 @@
+public class MyStepdefs {
+        @Given("^I have my matrix calculator$")
+        public void i_have_my_matrix_calculator() throws Throwable {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        }
+
+        @When("^I have entered matrix(\\d+) as first operand$")
+        public void i_have_entered_matrix_as_first_operand(int[][] arg1) throws Throwable {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        }
+
+        @When("^I have entered matrix(\\d+) as second operand$")
+        public void i_have_entered_matrix_as_second_operand(int[][] arg1) throws Throwable {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        }
+
+        @When("^I press 'CompareMatrix'$")
+        public void i_press_CompareMatrix() throws Throwable {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        }
+
+        @Then("^The result should be true$")
+        public void the_result_should_be_true() throws Throwable {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        }
+}
