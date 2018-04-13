b65baa25c3c57a706acb8135337ead0aebdeca9f
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-13 13:42:13 +0700

    Правильно реализовали метод CompareMatrix. Тест первого сценария успешно проходит

--- src/main/java/MatrixCalc.java
+++ src/main/java/MatrixCalc.java
@@ -1,7 +1,14 @@
 
 public class MatrixCalc {
-    public boolean CompareMatrix(int[][] operand1, int[][] operand2) {
-        return operand1.length == operand2.length;
+
+    public boolean CompareMatrix(int[][] a, int[][] b) {
+        if (a.length != b.length)
+            return false;
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[i].length; j++)
+                if ((a[i][j] != b[i][j]) || (a[i].length != b[i].length))
+                    return false;
+        return true;
     }
 
 }
