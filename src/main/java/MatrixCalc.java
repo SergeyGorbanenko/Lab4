758ee30fa00863f335054fab9ca4f4d75ae7cd6e
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-13 22:53:56 +0700

    Реализовали метод MatrixCalc.Mul(). Тест на умножение двух матриц успешно проходит

--- src/main/java/MatrixCalc.java
+++ src/main/java/MatrixCalc.java
@@ -42,4 +42,15 @@ public class MatrixCalc {
                 res[i][j] = a[i][j] * v;
         return res;
     }
+
+    public int[][] Mul(int[][] a, int[][] b) {
+        int[][] res = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[i].length; j++) {
+                res[i][j] = 0;
+                for (int k = 0; k < b[i].length; k++)
+                    res[i][j] += a[i][k] * b[k][j];
+            }
+        return res;
+    }
 }
