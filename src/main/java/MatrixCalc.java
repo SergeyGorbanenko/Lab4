827ff9648c4a55ecdf633a15acf268216ecdd2ca
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-13 22:12:19 +0700

    Реализовали метод MatrixCalc.Add(). Тест на сложение двух матриц успешно проходит

--- src/main/java/MatrixCalc.java
+++ src/main/java/MatrixCalc.java
@@ -18,4 +18,12 @@ public class MatrixCalc {
                 res[i][j] = a[j][i];
         return res;
     }
+
+    public int[][] Add(int[][] a, int[][] b) {
+        int[][] res = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[i].length; j++)
+                res[i][j] = a[i][j] + b[i][j];
+        return res;
+    }
 }
